5eafb13be3b8b08fc821c114a5d518dd78f79d42
skuzi123 <mateuszskuza10@spoko.pl>
2023-10-23 21:20:35 +0200

    [VolleyballApp-4] Add login and register page(logic should be repaired)

---
+++ webclient/src/app/core/register/register.constants.ts
@@ -0,0 +1,8 @@
+export const registerMessages = {
+  incorrectCredentials: 'Incorrect username and/or password',
+  username: 'Username',
+  fieldRequired: 'This field is required.',
+  password: 'Password',
+  role: 'Role',
+  signUp: 'Sign Up'
+}
